bdf19bf7bd7796294cd12af31bdab2b72f52d673
Wouter Molhoek <woutermolhoek@hotmail.com>
2019-04-18 20:50:13 +0200

    Get all the programming-lan from the repos

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 # Requested URL
-url = 'https://github.com/WouterMolhoek/'
+url = 'https://github.com/WouterMolhoek?tab=repositories'
 
 # Query the website (url) and return the html
 page = urllib.request.urlopen(url)
@@ -10,4 +10,9 @@ page = urllib.request.urlopen(url)
 # Parse the html using beautiful soup and store in variable `soup`
 soup = BeautifulSoup(page, 'html.parser')
 
-print(soup)
+# Get the programmingLanguage item property
+programming_lan = soup.find_all(itemprop='programmingLanguage')
+
+for language in programming_lan:
+    print(language.get_text())
+
