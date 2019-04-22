e649504b1b47032be841991253b61b2cdd1e3548
Wouter Molhoek <woutermolhoek@hotmail.com>
2019-04-22 13:39:01 +0200

    Added all the followers and following

--- script.py
+++ script.py
@@ -3,13 +3,17 @@ from bs4 import BeautifulSoup
 from fpdf import FPDF
 import webbrowser
 
-github = 'WouterMolhoek'
+github_account = 'WouterMolhoek'
+github_url = 'https://github.com/'
 
 # Repositories URL
-url_repos = 'https://github.com/' + github + '?tab=repositories'
+url_repos = github_url + github_account + '?tab=repositories'
 # Github Profile URL
-url_profile = 'https://github.com/' + github
-
+url_profile = github_url + github_account
+# Github Followers URL
+url_followers = github_url + github_account + '?tab=followers'
+# Github following URL
+url_following = github_url + github_account + '?tab=following'
 
 # Scrape data from the given URL
 def scrape_raw_data(url):
@@ -33,13 +37,18 @@ def format_req_data():
     # Get the username
     username = profile.find(itemprop='additionalName').get_text()
 
+    # Get the followers and following
+    nav_container = profile.find(class_='UnderlineNav user-profile-nav js-sticky top-0')
+    followers = nav_container.find_all(class_='Counter')[3].get_text()
+    following = nav_container.find_all(class_='Counter')[4].get_text()
+
     # Get the contribution activity
     contribution = profile.find(class_='f4 text-normal mb-2').get_text()
 
     # Get all the repositories on the first page
     repositories = repos.find_all(itemprop='owns')
 
-    return [profile_img, repositories, contribution, name, username]
+    return [profile_img, repositories, contribution, name, username, followers.lstrip(), following.lstrip()]
 
 
 def download_image(url):
@@ -55,61 +64,89 @@ def create_pdf():
 
     data = format_req_data()
 
+    # Formdat data
+    profile_img = data[0]
+    repositories = data[1]
+    contribution = data[2]
+    name = data[3]
+    username= data[4]
+    followers = data[5]
+    following = data[6]
+
     # Download profile image
-    download_image(data[0])
+    download_image(profile_img)
 
     # Add layout-image to the page
     pdf.image('img/layout-pdf.jpg', x=0, y=0, w=210)
 
     # Add profile-image to the page
-    pdf.image('img/profile-image.jpg', x=18, y=8, w=81, link=data[0])
-
-    # Add github-logo
-    pdf.image('img/github-logo.png', x=113, y=84, w=5, link=url_profile)
-    pdf.x = 118
-    pdf.y = 82
-    # Add git link
-    pdf.multi_cell(50, 10, '/ ' + data[4], 0, 'L')
+    pdf.image('img/profile-image.jpg', x=18, y=8, w=81, link=profile_img)
 
     # Add name
     pdf.x = 110
     pdf.y = 6
     pdf.set_font('Arial', 'B', 22)
-    pdf.multi_cell(80, 10, data[3], 0, 'L')
+    pdf.multi_cell(80, 10, name, 0, 'L')
 
     # Add username
     pdf.x = 110
     pdf.y = 14
     pdf.set_font('Arial', '', 18)
-    pdf.multi_cell(80, 10, data[4], 0, 'L')
+    pdf.multi_cell(80, 10, username, 0, 'L')
+
+    # Add github-logo
+    #pdf.image('img/github-logo.png', x=113, y=64, w=5, link=url_profile)
+    # Add git link (string)
+    #pdf.x = 118
+    #pdf.y = 62
+    #pdf.set_font('Arial', '', 12)
+    #pdf.multi_cell(50, 10, '/ ' + username, 0, 'L')
+
+    # Add followers button
+    pdf.image('img/button.png', x=107, y=73, w=45, h=22, link=url_followers)
+    # Add followers count
+    pdf.x = 112
+    pdf.y = 79.5
+    pdf.set_font('Arial', 'B', 12)
+    pdf.multi_cell(37, 10, 'Followers: ' + followers, 0, 'C')
+
+    # Add following button
+    pdf.image('img/button.png', x=152, y=73, w=45, h=22, link=url_following)
+    # Add followeing count
+    pdf.x = 156
+    pdf.y = 79.5
+    pdf.set_font('Arial', 'B', 12)
+    pdf.multi_cell(37, 10, 'Following: ' + following, 0, 'C')
 
     # Add 'Repositories' heading
     pdf.y = 120
-    repository_y = pdf.y
     pdf.x = 22
-    pdf.set_font('Arial', style='B')
+    repository_y = pdf.y
+    pdf.set_font('Arial', 'B', 18)
     pdf.multi_cell(50, 10, 'Repositories', 0, 'L')
 
-    # Define space between repository paragraphs
+    # Define space between the first repository paragraph and the 'Repositories' heading
     space = 15
 
-    for repository in data[1]:
+    for repository in repositories:
         # Repository title
         title = str(repository.find(itemprop='name codeRepository').get_text())
 
+        # Add the title
         pdf.x = 22
         pdf.y = repository_y + space
-        pdf.set_font('Arial')
+        title_y = pdf.y
+        pdf.set_font('Arial','', 14)
         pdf.multi_cell(70, 10, title.lstrip(), 0, 'L')
         space += 15
 
     # Add 'Contribution' heading
     pdf.x = 115
     pdf.y = 120
-    pdf.set_font('Arial', style='B')
+    pdf.set_font('Arial', 'B', 18)
     pdf.multi_cell(50, 10, 'Contribution', 0, 'L')
 
-    file_name = 'Github-Profile-(' + data[3] + ').pdf'
+    file_name = 'Github-Profile-(' + name + ').pdf'
 
     # Save the file (add profile name)
     pdf.output(file_name)
